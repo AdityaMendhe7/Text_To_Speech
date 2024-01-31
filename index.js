82fba30cb8346a2820c3c7b14047404427f2473d
AdityaMendhe7 <112185153+AdityaMendhe7@users.noreply.github.com>
2024-01-31 13:54:18 +0530

    lic audio

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function main() {
   console.log("Creating audios Please wait😮‍💨😮‍💨😮‍💨")
   for (let i = 0; i < json.length; i++) {
     const payload = {
-      Text: `${json[i].answers_asm}`
+      Text: `${json[i].answers_asm}`  // Audio text key
     };
 
     const result = await voiceapi(payload);
