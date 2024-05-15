8750ddbccc0ae2ec254adf36f2b91a3f9096def5
AdityaMendhe7 <112185153+AdityaMendhe7@users.noreply.github.com>
2024-05-15 11:49:07 +0530

    push

---
+++ cognative.js
@@ -0,0 +1,105 @@
+const axios = require("axios");
+const fs = require("fs");
+const json = require("./input.json");
+
+async function voiceapi(payload) {
+  let result;
+  const axiosConfig = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Connection: "keep-alive",
+      "auth-Key": "2b5fb5d4-0753-4302-b661-f8580e9effb0",
+      "sec-ch-ua-mobile": "?0",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+      "Content-Type": "application/json",
+      Accept: "application/json, text/plain, */*",
+      "Cache-Control": "max-age=31536000",
+      "app-id": "29fd4f94-f793-4227-9588-056b5ffb1318",
+      "Sec-Fetch-Site": "same-origin",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Dest": "empty",
+      Referer: "https://assistant.corover.mobi/irctc/chatbot.html",
+      "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    },
+  };
+
+  try {
+    const res = await axios.post(
+      "https://cognitive.service.corover.ai/dynamic/convertRealTimeAudioWav",
+      payload,
+      axiosConfig
+    );
+    result = res.data["Uploaded URL"];
+  } catch (error) {
+    console.error(error);
+    result = null;
+  }
+
+  return result;
+}
+async function BucketStore(result) {
+  try {
+    const response = await axios.get(
+      `https://cognitive.service.corover.ai/util/save?url=${result}`
+    );
+    return response.data["saved"];
+  } catch (error) {
+    console.error(error);
+    console.log("bucket api ERROR!!!");
+    throw error;
+  }
+}
+
+async function main() {
+  const output = [];
+  const answerOutput = [];
+
+  for (let i = 0; i < json.length; i++) {
+    let answerText = json[i].answers_kn;
+    // Remove specific HTML tags from the answer text
+    answerText = answerText.replace(/<br>/gi, "");
+    answerText = answerText.replace(/<b>/gi, "");
+    answerText = answerText.replace(/’/gi, "");
+    answerText = answerText.replace(/<a[^>]*>(.*?)<\/a>/gi, "$1");
+    answerText = answerText.replace(/\//g, "");
+    // answerText = answerText.replace(/-/g, "");
+    answerText = answerText.replace(/#N\/A/g, "");
+    answerText = answerText.replace(/<p><\/p>/gi, "");
+    answerText = answerText.replace(/<li><\/li>/gi, "");
+    answerText = answerText.replace(/\n/gi, "");
+
+    const payload = {
+      sourceText: answerText,
+      sourceLanguage: "ka",
+    };
+
+    const result = await voiceapi(payload);
+    const bucketstore = await BucketStore(result);
+    if (bucketstore) {
+      const audioUrl = bucketstore;
+      const audioNumber = i + 1;
+
+      // Display the console log with the numbered format and green Audio URL
+      console.log("%d Audio URL: \x1b[32m%s\x1b[0m", audioNumber, audioUrl);
+
+      output.push({
+        Answer: answerText,
+        Answer_audio: bucketstore,
+      });
+      // answerOutput.push([{
+      //   answer: {
+      //     contextCount: 1,
+      //     response: json[i].Response,
+      //     audio: result["Uploaded URL"],
+      //     options: [],
+      //   },
+      // }]);
+    }
+  }
+
+  fs.writeFileSync("CognativeOutput.json", JSON.stringify(output));
+  // fs.writeFileSync("Answer.json", JSON.stringify(answerOutput));
+}
+
+main();
---
+++ pmKisan.js
@@ -0,0 +1,51 @@
+
+const axios = require("axios");
+const fs = require("fs");
+const json = require("./input.json");
+
+async function voiceapi(payload) {
+  let result;
+  const axiosConfig = {
+    headers: {
+      "appId": "0708775d-c6af-4a88-ac47-346571727a0a",
+      "Content-Type": "application/json"
+    },
+  };
+
+  try {
+    const res = await axios.post(
+      "https://pmkisan.corover.ai/pmkisanAPI/nlp/VoiceApiBhashini/or",
+      payload,
+      axiosConfig
+    );
+    result = res.data;
+  } catch (error) {
+    console.error(error);
+    result = null;
+  }
+
+  return result;
+}
+
+async function main() {
+  const output = [];
+
+  console.log("Creating audios Please wait😮‍💨😮‍💨😮‍💨")
+  for (let i = 0; i < json.length; i++) {
+    const payload = {
+      Text: `${json[i].answers_or}`  // Audio text key // <==== changes
+    };
+
+    const result = await voiceapi(payload);
+    if (result) {
+      output.push({
+        input: payload.Text,
+        output: result,
+      });
+    }
+  } 
+
+  fs.writeFileSync("PmKisanOutput.json", JSON.stringify(output, null, 2));
+}
+
+main();
